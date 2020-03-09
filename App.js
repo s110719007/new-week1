bd5ed13b366f6d21352f5e2e714cb42c8d1ed6f8
Lillian <wlylillian@gmail.com>
2020-03-10 00:03:43 +0800

    week1

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110719007 王俐云 week1</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#CCFF99',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
